f8a57f5b3fd4a2ce68edb4ff7c1f1a963a089b67
Nik <nik.cimino@gmail.com>
2025-02-18 06:30:48 -0700

    feat: make wave into array on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -32,83 +32,69 @@ export class HomeComponent implements AfterViewInit, OnDestroy {
     const sketch = (s: p5) => {
       const xspacing = 16;
       let w: number;
-      let theta = 0.0;
-      let amplitude = 75.0;
       const period = 500.0;
       let dx: number;
-      let yvalues: number[];
+
+      const waves = [
+        { phase: 0, minAmplitude: -50, maxAmplitude: 50, minSpeed: 0.02, maxSpeed: -0.1, yvalues: [] as number[] },
+        { phase: 0, minAmplitude: -30, maxAmplitude: 30, minSpeed: 0.03, maxSpeed: 0.12, yvalues: [] as number[] },
+        { phase: 0, minAmplitude: -60, maxAmplitude: 60, minSpeed: 0.015, maxSpeed: 0.08, yvalues: [] as number[] },
+        { phase: 0, minAmplitude: -40, maxAmplitude: 40, minSpeed: 0.015, maxSpeed: -0.08, yvalues: [] as number[] },
+      ];
 
       s.setup = () => {
         const containerWidth = this.p5Container.nativeElement.offsetWidth;
-        const containerHeight = this.p5Container.nativeElement.offsetHeight;
+        const containerHeight = 500; //this.p5Container.nativeElement.offsetHeight;
         const canvas = s.createCanvas(containerWidth, containerHeight);
-        // const canvas = s.createCanvas(s.windowWidth, 200);
         canvas.parent(this.p5Container.nativeElement);
-        w = s.width + 16;
+        w = s.width + xspacing;
         dx = (s.TWO_PI / period) * xspacing;
-        yvalues = new Array(Math.floor(w / xspacing));
+        waves.forEach((wave) => (wave.yvalues = new Array(Math.floor(w / xspacing))));
       };
 
       s.draw = () => {
         s.clear();
-        // s.background(1);
-        // Calculate distance from the vertical center of the canvas
-        // const distFromCenter = Math.abs(s.mouseY - s.height / 2);
-        // Map the distance to amplitude:
-        // When the mouse is at the center (distFromCenter = 0), amplitude will be 500.
-        // When the mouse is at the top or bottom (distFromCenter = s.height / 2), amplitude will be 100.
-        // amplitude = s.map(distFromCenter, 0, s.height / 2, 400, 10);
-        amplitude = s.map(s.mouseY, 0, s.height, 200, -200);
-        // amplitude = s.map(s.mouseX, 0, s.width, 100, 500);
+        waves.forEach((wave) => {
+          // Map mouseY to amplitude
+          const currentAmplitude = s.map(s.mouseY, 0, s.height, wave.maxAmplitude, wave.minAmplitude);
+          // Map distance from center to speed
+          const distFromCenter = Math.abs(s.mouseX - s.width / 2);
+          const currentSpeed = s.map(distFromCenter, s.width / 2, 0, wave.maxSpeed, wave.minSpeed);
 
-        // Calculate the distance from the center of the canvas
-        const distFromCenter = Math.abs(s.mouseX - s.width / 2);
-        // Map the distance to a speed value
-        const speed = s.map(distFromCenter, 0, s.width / 2, 0.1, 0.02);
-        theta += speed;
-
-        calcWave();
-        renderWave();
+          wave.phase += currentSpeed;
+          calcWave(wave, currentAmplitude);
+          renderWave(wave);
+        });
       };
 
-      function calcWave() {
-        theta += 0.02;
-        let x = theta;
-        for (let i = 0; i < yvalues.length; i++) {
-          yvalues[i] = s.sin(x) * amplitude;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      function calcWave(wave: any, amplitude: number) {
+        let x = wave.phase;
+        for (let i = 0; i < wave.yvalues.length; i++) {
+          wave.yvalues[i] = s.sin(x) * amplitude;
           x += dx;
         }
       }
 
-      function renderWave() {
-        s.noStroke();
-        s.fill(255, 100);
-        // for (let i = 0; i < yvalues.length; i++) {
-        //   s.ellipse(i * xspacing, s.height / 2 + yvalues[i], 16, 16);
-        // }
-        for (let i = 0; i < yvalues.length; i++) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      function renderWave(wave: any) {
+        for (let i = 0; i < wave.yvalues.length; i++) {
           const x = i * xspacing;
-          // Calculate the distance to the nearest horizontal edge:
           const distanceToEdge = Math.min(x, s.width - x);
-          // Map the distance to an alpha value.
-          // When distanceToEdge is 0 (at the edge), set a low alpha (say 50)
-          // When distanceToEdge is half the canvas width (center-ish), set a high alpha (255)
           const alpha = s.map(distanceToEdge, 0, s.width / 3, 0, 20);
           s.noStroke();
-          // Use fill with an alpha value. (You can change the RGB values to your liking)
           s.fill(100, 100, 100, alpha);
-          s.ellipse(x, s.height / 2 + yvalues[i], 16, 16);
+          s.ellipse(x, s.height / 2 + wave.yvalues[i], 16, 16);
         }
       }
 
       s.windowResized = () => {
         const containerWidth = this.p5Container.nativeElement.offsetWidth;
-        const containerHeight = this.p5Container.nativeElement.offsetHeight;
+        const containerHeight = 500; //this.p5Container.nativeElement.offsetHeight;
         s.resizeCanvas(containerWidth, containerHeight);
-        // s.resizeCanvas(s.windowWidth, 200);
-        w = s.width + 16;
+        w = s.width + xspacing;
         dx = (s.TWO_PI / period) * xspacing;
-        yvalues = new Array(Math.floor(w / xspacing));
+        waves.forEach((wave) => (wave.yvalues = new Array(Math.floor(w / xspacing))));
       };
     };
 
