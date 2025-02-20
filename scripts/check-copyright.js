4054cb7ed85e693920d341f36fab41483d95a27f
Nik <nik.cimino@gmail.com>
2025-02-19 18:46:52 -0700

    chore: add copyrights and checks

--- scripts/check-copyright.js
+++ scripts/check-copyright.js
@@ -3,21 +3,47 @@ const fs = require('fs');
 const path = require('path');
 
 const currentYear = new Date().getFullYear();
-const COPYRIGHT_NOTICE_REGEX = new RegExp(`^// Copyright \\(c\\) 20\\d{2}-${currentYear} Perpetuator LLC$`);
-const COPYRIGHT_NOTICE_CURRENT_YEAR = `// Copyright (c) ${currentYear} Perpetuator LLC`;
+const COPYRIGHT_NOTICE_REGEX_JS_TS = new RegExp(`^\\/\\/ Copyright \\(c\\) 20\\d{2}-${currentYear} Perpetuator LLC$`);
+const COPYRIGHT_NOTICE_REGEX_HTML_MD = new RegExp(
+  `^<!-- Copyright \\(c\\) 20\\d{2}-${currentYear} Perpetuator LLC -->$`,
+);
+const COPYRIGHT_NOTICE_REGEX_CSS = new RegExp(
+  `^\\/\\* Copyright \\(c\\) 20\\d{2}-${currentYear} Perpetuator LLC \\*\\/$`,
+);
+const COPYRIGHT_NOTICE_CURRENT_YEAR_JS_TS = `// Copyright (c) ${currentYear} Perpetuator LLC`;
+const COPYRIGHT_NOTICE_CURRENT_YEAR_HTML_MD = `<!-- Copyright (c) ${currentYear} Perpetuator LLC -->`;
+const COPYRIGHT_NOTICE_CURRENT_YEAR_CSS = `/* Copyright (c) ${currentYear} Perpetuator LLC */`;
 
 function checkCopyright(filePath) {
   const content = fs.readFileSync(filePath, 'utf8');
-  if (!content.startsWith(COPYRIGHT_NOTICE_CURRENT_YEAR) && !COPYRIGHT_NOTICE_REGEX.test(content)) {
-    console.error(`Missing or incorrect copyright notice in ${filePath}`);
-    process.exit(1);
+  if (filePath.endsWith('.html') || filePath.endsWith('.md')) {
+    if (!content.startsWith(COPYRIGHT_NOTICE_CURRENT_YEAR_HTML_MD) && !COPYRIGHT_NOTICE_REGEX_HTML_MD.test(content)) {
+      console.error(`Missing or incorrect copyright notice in ${filePath}`);
+      process.exit(1);
+    }
+  } else if (filePath.endsWith('.css') || filePath.endsWith('.scss')) {
+    if (!content.startsWith(COPYRIGHT_NOTICE_CURRENT_YEAR_CSS) && !COPYRIGHT_NOTICE_REGEX_CSS.test(content)) {
+      console.error(`Missing or incorrect copyright notice in ${filePath}`);
+      process.exit(1);
+    }
+  } else if (filePath.endsWith('.ts') || filePath.endsWith('.js')) {
+    if (!content.startsWith(COPYRIGHT_NOTICE_CURRENT_YEAR_JS_TS) && !COPYRIGHT_NOTICE_REGEX_JS_TS.test(content)) {
+      console.error(`Missing or incorrect copyright notice in ${filePath}`);
+      process.exit(1);
+    }
   }
 }
 
 const files = process.argv.slice(2);
 files.forEach((file) => {
-  if (file.endsWith('.ts') || file.endsWith('.js')) {
-    // console.log('Checking', file);
+  if (
+    file.endsWith('.ts') ||
+    file.endsWith('.js') ||
+    file.endsWith('.html')
+    // || file.endsWith('.md')
+    // || file.endsWith('.css')
+    // || file.endsWith('.scss')
+  ) {
     checkCopyright(file);
   }
 });
