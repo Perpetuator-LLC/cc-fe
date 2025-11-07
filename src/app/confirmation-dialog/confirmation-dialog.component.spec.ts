b7e641789c81623d820769a25973742f7b981bde
Nik <nik.cimino@gmail.com>
2025-11-06 20:34:00 -0700

    test: fix several tests

--- src/app/confirmation-dialog/confirmation-dialog.component.spec.ts
+++ src/app/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -1,15 +1,22 @@
 // Copyright (c) 2025 Perpetuator LLC
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ConfirmationDialogComponent } from './confirmation-dialog.component';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 describe('ConfirmationDialogComponent', () => {
   let component: ConfirmationDialogComponent;
   let fixture: ComponentFixture<ConfirmationDialogComponent>;
+  let mockDialogRef: jasmine.SpyObj<MatDialogRef<ConfirmationDialogComponent>>;
 
   beforeEach(async () => {
+    mockDialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
+
     await TestBed.configureTestingModule({
       imports: [ConfirmationDialogComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: mockDialogRef },
+        { provide: MAT_DIALOG_DATA, useValue: { message: 'Test message' } },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ConfirmationDialogComponent);
--- src/app/job.service.spec.ts
+++ src/app/job.service.spec.ts
@@ -1,13 +1,27 @@
 // Copyright (c) 2025 Perpetuator LLC
 import { TestBed } from '@angular/core/testing';
-
+import { Apollo } from 'apollo-angular';
 import { JobService } from './job.service';
+import { ErrorHandlerService } from './error-handler.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('JobService', () => {
   let service: JobService;
+  let mockApollo: jasmine.SpyObj<Apollo>;
+  let mockErrorHandler: jasmine.SpyObj<ErrorHandlerService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockApollo = jasmine.createSpyObj('Apollo', ['query', 'mutate', 'watchQuery']);
+    mockErrorHandler = jasmine.createSpyObj('ErrorHandlerService', ['handleError']);
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        JobService,
+        { provide: Apollo, useValue: mockApollo },
+        { provide: ErrorHandlerService, useValue: mockErrorHandler },
+      ],
+    });
     service = TestBed.inject(JobService);
   });
 
