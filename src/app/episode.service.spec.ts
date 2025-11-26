6155bc3325576eb111fed26df3f62304d7b04964
Nik <nik.cimino@gmail.com>
2025-11-26 13:12:59 -0500

    test: add mock dependencies to service test suites
    Add Apollo and ErrorHandlerService mocks to all service test files to fix
    dependency injection errors. This ensures tests can properly instantiate
    services that depend on these providers.
    
    Configure mock Apollo methods (query, mutate, watchQuery) to return
    empty observables, providing minimal test doubles for GraphQL operations.
    
    Add provideHttpClient to HomeComponent test to satisfy HTTP dependencies.
    
    Update API key header format in documentation from 'ApiKey' to 'X-API-Key'
    for consistency with standard header naming conventions.

--- src/app/episode.service.spec.ts
+++ src/app/episode.service.spec.ts
@@ -1,13 +1,29 @@
 // Copyright (c) 2025 Perpetuator LLC
 import { TestBed } from '@angular/core/testing';
-
+import { Apollo } from 'apollo-angular';
+import { of } from 'rxjs';
 import { EpisodeService } from './episode.service';
+import { ErrorHandlerService } from './error-handler.service';
 
 describe('EpisodeService', () => {
   let service: EpisodeService;
+  let mockApollo: jasmine.SpyObj<Apollo>;
+  let mockErrorHandler: jasmine.SpyObj<ErrorHandlerService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockApollo = jasmine.createSpyObj('Apollo', ['query', 'mutate', 'watchQuery']);
+    mockErrorHandler = jasmine.createSpyObj('ErrorHandlerService', ['handleError']);
+
+    mockApollo.query.and.returnValue(of({ data: {} }));
+    mockApollo.mutate.and.returnValue(of({ data: {} }));
+
+    TestBed.configureTestingModule({
+      providers: [
+        EpisodeService,
+        { provide: Apollo, useValue: mockApollo },
+        { provide: ErrorHandlerService, useValue: mockErrorHandler },
+      ],
+    });
     service = TestBed.inject(EpisodeService);
   });
 
